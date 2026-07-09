33d78aafcd97e2cd8f639615a92429fe00f4edbe
2ksllvwr <nascimentogustavoti@gmail.com>
2026-07-09 18:18:09 -0300

    test: apply Bernadette brand font

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "denarius-shell-v3";
-const SHELL_ASSETS = ["/", "/manifest.webmanifest", "/icon.svg", "/fonts/avenue-de-madison.ttf"];
+const SHELL_ASSETS = ["/", "/manifest.webmanifest", "/icon.svg", "/fonts/bernadette.ttf"];
 
 self.addEventListener("install", event => {
   event.waitUntil(
